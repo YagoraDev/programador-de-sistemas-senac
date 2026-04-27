541223412a708aa04952707fd3498d51bcdae092
Yago Siqueira <yago63217656@edu.pa.senac.br>
2026-04-27 09:40:24 -0300

    Primeiro commit

---
+++ conteudo/aula1.py
@@ -0,0 +1,16 @@
+idade = 20
+salario = 5800.50
+altura = 1.63
+genero = "F"
+nome = 'Maria Silva'
+
+print(type(idade))
+print(type(salario))
+print(type(altura))
+print(type(genero))
+print(type(nome))
+
+# print(idade)
+# print('Idade = ',idade)
+# sintaxes de saída de dados
+# print("A funcionária",nome,"\ntem",idade,"\ndo sexo")
