cf5dd6305c75353b21ac433e589ae2b7ada3d762
Yago Siqueira <yagosiqueiraa1@gmail.com>
2026-04-29 11:34:57 -0300

    Adiciona Aula 2 e desafio

---
+++ aula2.py
@@ -0,0 +1,13 @@
+# Entrada de dados pelo usuário
+nome = input('Informe seu nome: ')
+# Saida
+print(f'O nome informado foi {nome}')
+
+idade = input('Qual sua idade: ')
+print(f'O usuário {nome} tem {idade} anos')
+print(type(nome))
+print(type(idade))
+
+num1 = int(input("Informe um número"))
+num2 = int(input("informe outro número"))
+print(f'A soma de {num1} + {num2} = {num1 + num2}')
---
+++ desafio-catalogo-de-streaming.py
@@ -0,0 +1,54 @@
+# nomeDoFilme = input('Insira o nome do filme: ')
+# anoDoFilme  = int(input('Insira o ano de lançamento do filme: '))
+# notaDoFilme = float(input('Insira uma nota de 0 a 10: '))
+# disponibilidadeDoFilme = bool(input('Escreva true para filme disponivel ou false para filme indisponivel: '))
+
+# print(f'Nome do filme: {nomeDoFilme} \n',
+#       f'Ano de lançamento: {anoDoFilme} \n',
+#       f'nota: {notaDoFilme} \n',
+#       f'Disponível: {disponibilidadeDoFilme}')
+
+
+
+# nomeDoFilme = "Oppenheirmer"
+# anoDoFilme  = 2023
+# notaDoFilme = 8.4
+# disponibilidadeDoFilme = True
+
+# print("Filme: ",nomeDoFilme)
+# print("Abo: ", anoDoFilme)
+# print("nota: ", notaDoFilme)
+# print("Disponível: ", disponibilidadeDoFilme)
+
+#LOJA DE ROUPA
+produto1 = "Blusa"
+produto2 = "Calça"
+
+precoProduto1 = 50.00
+precoProduto2 = 70.00
+
+identificacaoProduto1 = 1
+identificacaoProduto2 = 2
+
+nomeCliente1 = "Sarah"
+nomeCliente2 = "João"
+
+idadeCliente1 = 24
+idadeCliente2 = 50
+
+generoCliente1 = "Feminino"
+generoCliente2 = "Masculino"
+
+
+print()
+print(f"Produtos da loja: {produto1}, {produto2}")
+print()
+print(f"Preço da Blusa: {precoProduto1}")
+print(f"Preço da calça: {precoProduto2}")
+print()
+print(f"Código de verificação da {produto1}: {identificacaoProduto1}")
+print(f"Código de verificação da {produto2}: {identificacaoProduto2}")
+print()
+print(f"Nome do cliente cadastrado: {nomeCliente1}, idade: {idadeCliente1}, gênero: {generoCliente1}")
+print()
+
