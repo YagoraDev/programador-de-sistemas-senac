683690a21e4827638407a5dae35cfd4786692d42
Yago Siqueira <yagosiqueiraa1@gmail.com>
2026-05-20 11:38:46 -0300

    Adiciona modificacoes da aula 5

--- fundamentos-python-1/aula5.py
+++ fundamentos-python-1/aula5.py
@@ -1,20 +1,25 @@
 # TABUADA USANDO FOR
 
-num = int(input("Escolha o número da tabuada:"))
-operacao = input("Escolha a operação ( + , - , * , / ): ")
-if operacao == "+":
-    print("Tabuada de Adição")
-    for c in range(1,11):
-        print(f"{num} + {c} = {num+c}")
-elif operacao == "-":
-    print("Tabuada de Subtração")
-    for c in range(1,11):
-        print(f"{num} - {c} = {num-c}")
-elif operacao == "*":
-    print("Tabuada da Multiplicação")
-    for c in range(1,11):
-        print(f"{num} x {c} = {num*c}")
-elif operacao == "/":
-    print("Tabuada de Divisão")
-    for c in range(1,11):
-        print(f"{num} / {c} = {num/c:.1f}")
+while True:
+    num = int(input("Escolha o número da tabuada:"))
+    operacao = input("Escolha a operação ( + , - , * , / ): ")
+    if operacao == "+":
+        print("Tabuada de Adição")
+        for c in range(1,11):
+            print(f"{num} + {c} = {num+c}")
+    elif operacao == "-":
+        print("Tabuada de Subtração")
+        for c in range(1,11):
+            print(f"{num} - {c} = {num-c}")
+    elif operacao == "*":
+        print("Tabuada da Multiplicação")
+        for c in range(1,11):
+            print(f"{num} x {c} = {num*c}")
+    elif operacao == "/":
+        print("Tabuada de Divisão")
+        for c in range(1,11):
+            print(f"{num} / {c} = {num/c:.1f}")
+    else: print("Operador inválido")
+    resposta = input("Deseja continuar? (s/n):").strip().lower()[0]
+    if resposta == "n":
+        break
