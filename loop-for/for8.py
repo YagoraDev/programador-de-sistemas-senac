a736b928d02b1fc0f40ed39bbe6d221138172a26
Yago Siqueira <yagosiqueiraa1@gmail.com>
2026-05-20 11:14:01 -0300

    Adiciona aula 8 de tabuada

---
+++ loop-for/for8.py
@@ -0,0 +1,18 @@
+# TABUADA USANDO FOR
+# print("Tabuada de subtração")
+# num = int(input("Escolha o número da tabuada:"))
+# for c in range(1,11):
+#     print(f"{num} - {c} = {num-c}")
+
+# num = int(input("Escolha o número da tabuada:"))
+# for c in range(1,11):
+#     print(f"{num} x {c} = {num*c}")
+
+# num = int(input("Escolha o número da tabuada:"))
+# for c in range(1,11):
+#     print(f"{num} + {c} = {num+c}")
+
+num = int(input("Escolha o número da tabuada:"))
+for c in range(1,11):
+    print(f"{num} / {c} = {num/c:.1f}")
+
