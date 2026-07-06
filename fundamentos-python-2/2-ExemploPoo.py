7ffae155a7755b69adb0947d56786e2a136ebd56
Yago Siqueira <yagosiqueiraa1@gmail.com>
2026-07-06 10:48:57 -0300

    Adiciona alterações

--- fundamentos-python-2/2-ExemploPoo.py
+++ fundamentos-python-2/2-ExemploPoo.py
@@ -53,6 +53,7 @@ class Cliente:
             print("Código do cliente não encontrado.")
 
 # Cliente1 precisa ser da classe cliente para utilizar os métodos da classe.
+# Criando um cliente (Objeto)
 # cliente1 = Cliente() 
 # cliente1.cadastrar_cliente()
 # cliente1.mostrar_dados()
@@ -81,17 +82,69 @@ class Cliente:
 # Lista para armazenar vários clientes
 clientes = []
 
-# Loop para cadastrar múltiplos clientes
-quantidade = int(input("Quantos clientes deseja cadastrar? "))
+# # Loop para cadastrar múltiplos clientes
+# quantidade = int(input("Quantos clientes deseja cadastrar? "))
 
-for c in range(quantidade):
-    print(f"\nCadastro do cliente {c+1}:")
-    cliente = Cliente()         # Cria um novo cliente vazio
-    cliente.cadastrar_cliente()
-    clientes.append(cliente)    # Adiciona na lista
+# for c in range(quantidade):
+#     print(f"\nCadastro do cliente {c+1}:")
+#     cliente = Cliente()         # Cria um novo cliente vazio
+#     cliente.cadastrar_cliente()
+#     clientes.append(cliente)    # Adiciona na lista
 
 
-# Mostrar todos os clientes cadastrados
-print("\n--- Lista de Clientes Cadastrados ---")
-for cliente in clientes:
-    cliente.mostrar_dados()
+# # Mostrar todos os clientes cadastrados
+# print("\n--- Lista de Clientes Cadastrados ---")
+# for cliente in clientes:
+#     cliente.mostrar_dados()
+
+
+# Função para buscar cliente pelo código
+def buscar_cliente(codigo):
+    for cliente in clientes:
+        if cliente.codigo == codigo:
+            return cliente
+    return None
+
+
+
+# Menu principal
+while True:
+    print("\n--- MENU DE CLIENTES ---")
+    print("1 - Cadastrar clientes")
+    print("2 - Listar clientes")
+    print("3 - Atualizar clientes")
+    print("4 - Excluir clientes")
+    print("5 -  Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        cliente = Cliente()
+        cliente.cadastrar_cliente()
+        clientes.append(cliente)
+
+    elif opcao == "2":
+        if clientes:
+            print("\n--- Lista de Clientes")
+            for cliente in clientes:
+                cliente.mostrar_dados()
+        else:
+            print("Nenhum cliente cadastrado. \n")
+    
+    elif opcao == "3":
+        codigo = input("Digite o código do cliente")
+        cliente = buscar_cliente(codigo)
+        if cliente:
+            cliente.atualizar_cliente()
+        else:
+            print("Cliente não encontrado. \n")
+
+    elif opcao == "4":
+        cliente.excluir_cliente()
+        clientes.remove(cliente)
+    
+    elif opcao == "5":
+        print("Saindo do sistema...")
+
+    else:
+        print("Opção inválida. Tente novamente. \n")
